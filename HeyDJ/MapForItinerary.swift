9b79c7bbd0bf4d0895d8a9fb653617e8c330f162
Pankajr <pankajr@raghav-imac.local>
2018-06-19 13:01:38 +0530

    third commit

---
+++ HeyDJ/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  HeyDJ
+//
+//  Created by Pankajr on 19/06/18.
+//  Copyright © 2018 AppyPie. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
